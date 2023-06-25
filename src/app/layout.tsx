0f1f9842eba71bd91cd87eac3e5cf0aa9fbd8ed6
Krushil <krushilnaik96@gmail.com>
2023-06-25 19:15:10 -0400

    Add hover animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,17 @@
 
 import { useState } from "react";
 import "./globals.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const [open, setOpen] = useState(false);
+  const pathname = usePathname();
 
   return (
     <html lang="en">
       <body className="grid grid-rows-[auto_1fr_auto] min-h-screen">
-        <header className="h-16 bg-amber-400 text-slate-900 p-4 flex justify-end">
+        <header className="h-16 bg-amber-400 text-slate-900 p-4 flex justify-end ">
           <button className="relative text-2xl z-50" onClick={() => setOpen(!open)}>
             x
           </button>
@@ -17,8 +20,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             style={{ top: open ? 0 : "-100%" }}
             className="absolute left-0 bg-amber-300 w-full h-screen grid items-center justify-center text-6xl transition-all duration-500"
           >
-            <li>Home</li>
-            <li>About</li>
+            <li>
+              <Link href={"/"} className={pathname === "/" ? "active" : ""}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href={"/about"} className={pathname === "/about" ? "active" : ""}>
+                About
+              </Link>
+            </li>
             <li>Contact</li>
           </ul>
         </header>
